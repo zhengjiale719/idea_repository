de9aa3a2019e1c0674f1d1b778bd474eba457bae
zjl <1583078628@qq.com>
2019-12-21 15:17:37 +0800

    aliyun_sms

---
+++ src/main/java/SendSms.java
@@ -0,0 +1,34 @@
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+
+public class SendSms {
+    public static void main(String[] args) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4FvBkmf5QxEEciR5nXz8", "Y1pUBHTmX4TFpHkUgaaOyJoIkiFIos");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", "18777984010");
+        request.putQueryParameter("SignName", "xCheckCodex");
+        request.putQueryParameter("TemplateCode", "SMS_174811077");
+        request.putQueryParameter("TemplateParam", "{'code':''}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
